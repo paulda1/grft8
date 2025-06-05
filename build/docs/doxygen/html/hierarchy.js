f9bbf7042e59453fe0c7163eb7a018fd8bcb307a
Marcus Müller <mueller@baseband.digital>
2025-06-05 19:39:48 +0200

    Don't include build/ in the git repo, put it in ignore list
    Signed-off-by: Marcus Müller <mueller@baseband.digital>

--- build/docs/doxygen/html/hierarchy.js
+++
@@ -1,8 +0,0 @@
-var hierarchy =
-[
-    [ "sync_block", null, [
-      [ "gr::ft8::encoder", "classgr_1_1ft8_1_1encoder.html", [
-        [ "gr::ft8::encoder_impl", "classgr_1_1ft8_1_1encoder__impl.html", null ]
-      ] ]
-    ] ]
-];
