2c13f3d5a0e5ecfcb534b5af7f3ca9fdd5174167
Daniel Paul <pauld1@myumanitoba.ca>
2025-07-24 21:52:45 +0200

    fsk

--- lib/encoder_impl.h
+++ lib/encoder_impl.h
@@ -8,42 +8,38 @@
 #ifndef INCLUDED_FT8_ENCODER_IMPL_H
 #define INCLUDED_FT8_ENCODER_IMPL_H
 
-#include <gnuradio/ft8/encoder.h>
 #include "message.h"
+#include <algorithm>
+#include <bitset>
+#include <gnuradio/ft8/encoder.h>
 #include <string>
 #include <vector>
-#include <algorithm>  
-#include <bitset>   
 
 namespace gr {
-  namespace ft8 {
+namespace ft8 {
 
-    class encoder_impl : public encoder
-    {
-     private:
-        message d_message_obj;
-        std::vector<float> d_waveform;
-        size_t d_sample_idx;
-        bool d_waveform_generated;
+class encoder_impl : public encoder {
+   private:
+    message d_message_obj;
+    std::vector<float> d_waveform;
+    size_t d_sample_idx;
+    bool d_waveform_generated;
 
-        void generate_waveform();
-        std::vector<float> generate_rectangular_fsk(const std::vector<int>& symbols);
-        
-     public:
-      encoder_impl(std::string message_text);
-      ~encoder_impl();
+    void generate_waveform();
+    std::vector<float> generate_freq_deviation(const std::vector<int>& symbols);
 
-      message::message_type get_message_type();
-      const std::string& get_processed_message();
+   public:
+    encoder_impl(std::string message_text);
+    ~encoder_impl();
 
-      int work(
-              int noutput_items,
-              gr_vector_const_void_star &input_items,
-              gr_vector_void_star &output_items
-      );
-    };
+    message::message_type get_message_type();
+    const std::string& get_processed_message();
 
-  } // namespace ft8
+    int work(int noutput_items, gr_vector_const_void_star& input_items,
+             gr_vector_void_star& output_items);
+};
+
+} // namespace ft8
 } // namespace gr
 
 #endif /* INCLUDED_FT8_ENCODER_IMPL_H */
